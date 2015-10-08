6455d2060875087470e46af2ba9340b15956b76b
bhuvanabellala1 <bbella1@berkeley.edu>
2015-10-07 20:20:44 -0700

    Played around with jcrew.py to only get the header text and details. Did not succeed. Added a call to findSales in app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from jcrew import findSales
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    findSales("https://www.jcrew.com/index.jsp")
     return 'Hello World!'
 
 if __name__ == '__main__':
--- jcrew.py
+++ jcrew.py
@@ -1,32 +1,17 @@
-__author__ = 'nataliegoldberg'
-
-
-import urllib
-import time
 from bs4 import BeautifulSoup
 import re
 import requests
 
 
 def findSales(url=None):
-    counter = 0
 
     if url != None:
-        counter += 1
-        tempSnapshot = {}
-        #print
-
         try:
             jcrewResponse = requests.get(url)
-            timeStamp = time.time()
-            tempSnapshot['timeStamp'] = timeStamp
-            print "Time Stamp:", timeStamp
             html = jcrewResponse.text
-            #print html
             soup = BeautifulSoup(html, "html.parser")
-            # get sale
-            for i in soup.find_all('div', {'id': 'globalHeaderPromoContainer'}):
-                print i
+            for child in soup.find('div', {'id': 'globalHeaderPromoContainer'}).descendants:
+                print child
 
         except:
             print "Error getting sale info!"
