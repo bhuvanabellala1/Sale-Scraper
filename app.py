382d714acff16ef8e441b2abbbfe3f634b47dc63
bhuvanabellala1 <bbella1@berkeley.edu>
2015-11-03 15:52:39 -0800

    Added Ajax request to get sale data from server

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+import json
+import flask
+from flask import Flask, jsonify, Response, request
 from jcrew import findSales
+
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    promoText, details = findSales("https://www.jcrew.co/index.jsp")
-    print "here"
-    if promoText:
-        return promoText
-    else:
-        return "Failed"
+    return flask.render_template('index.html')
+
+@app.route('/deals', methods=['GET', 'POST'])
+def get_deals():
+    return jsonify(jcrew="30% off", myRes="hedslo",)
+
 
 if __name__ == '__main__':
     app.run()
