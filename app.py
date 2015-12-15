47c644794db0ee95e27da1ea7d9a9e442e76b2c7
Natalie Goldberg <nataliegoldberg@unknownc48508ad5903.att.net>
2015-12-14 16:25:16 -0800

    updated bags

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def get_deals():
 
     deals = []
     promoText, details = findSales("jcrew", "https://www.jcrew.com/index.jsp")
-    deals.append({'company': 'jcrew', 'image': 'static/jcrew.png', 'promo' : promoText, 'details': details})
+    deals.append({'company': 'jcrew', 'image': 'static/jcrew.jpg', 'promo' : promoText, 'details': details})
     promoText, details = findSales("nyc", "http://www.nyandcompany.com/nyco/")
-    deals.append({'company': 'nyc', 'image': 'static/nyc.png', 'promo' : promoText, 'details': details})
+    deals.append({'company': 'nyc', 'image': 'static/nyco.jpg', 'promo' : promoText, 'details': details})
     promoText, details = findSales("vra", "http://www.verabradley.com/")
     deals.append({'company': 'vra', 'image': 'static/vra.jpg', 'promo' : promoText, 'details': details})
 
